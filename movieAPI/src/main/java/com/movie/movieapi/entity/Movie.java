427da272e29e6ba9c5e0effa9f6023a5ad6da0b2
SNKURUVI <snehakuruvilla4@gmail.com>
2022-10-25 16:57:19 +0200

    commit v4_25/10/2022 Api to get all movies

--- movieAPI/src/main/java/com/movie/movieapi/entity/Movie.java
+++ movieAPI/src/main/java/com/movie/movieapi/entity/Movie.java
@@ -1,21 +1,35 @@
 package com.movie.movieapi.entity;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "movie")
 @Data
-public class Movie {
+public class Movie implements Serializable {
 
     @Id
+    @Column(name = "movie_id")
     private Long movieid;
+
+    @Column(name = "movie_name")
     private String movieName;
+
+    @Column(name = "box_office_rate")
     private double boxOfficeRate;
+
+    @Column(name = "release_year")
     private int releaseYear;
+
+    @Column(name = "language")
     private String language;
+
+    @Column(name = "rated")
     private String rated;
 }
--- movieAPI/src/main/java/com/movie/movieapi/serviceImpl/MovieServiceImpl.java
+++ movieAPI/src/main/java/com/movie/movieapi/serviceImpl/MovieServiceImpl.java
@@ -5,6 +5,8 @@ import com.movie.movieapi.entity.Movie;
 import com.movie.movieapi.mapper.MovieMapper;
 import com.movie.movieapi.repository.MovieRepository;
 import com.movie.movieapi.service.MovieService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,13 @@ import java.util.List;
 @Slf4j
 public class MovieServiceImpl implements MovieService {
 
-    @Autowired
-    MovieRepository movieRepository;
-
+    private MovieRepository movieRepository;
     private MovieMapper movieMapper;
+
+    public MovieServiceImpl(MovieRepository movieRepository,MovieMapper movieMapper) {
+        this.movieRepository = movieRepository;
+        this.movieMapper = movieMapper;
+       }
 
     /**
      * @return
@@ -27,6 +32,7 @@ public class MovieServiceImpl implements MovieService {
     public List<MovieDto> getAllMovies() {
         log.info("Entering the getAllMovies of MovieServiceImpl class!!");
         List<Movie> movieList = movieRepository.findAll();
+        log.info("snehaaaaaaa");
         return movieMapper.toDto(movieList);
     }
 }
