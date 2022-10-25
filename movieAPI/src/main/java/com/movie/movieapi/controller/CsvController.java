30ffd2dcdadb3dc75567a9c47d836ad9446d765a
AdamMokhtar <adammokhtar10@gmail.com>
2022-10-25 15:29:48 +0200

    added CSV handler

--- movieAPI/src/main/java/com/movie/movieapi/controller/CsvController.java
+++ movieAPI/src/main/java/com/movie/movieapi/controller/CsvController.java
@@ -1,4 +1,34 @@
 package com.movie.movieapi.controller;
 
+import com.movie.movieapi.service.CsvService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/csv")
 public class CsvController {
+
+    @Autowired
+    private CsvService csvService;
+
+    @GetMapping("BestPicNom")
+    public String getBestPicNom() throws URISyntaxException, IOException {
+        return csvService.getBestPictureNominees();
+    }
+
+    @GetMapping("BestPicWinners")
+    public String getBestPicWin() throws URISyntaxException, IOException {
+        return csvService.getBestPictureWinners();
+    }
+
+
+    @GetMapping("BestPicWon/{movieName}")
+    public String checkBestPicWin(@PathVariable("movieName") String movieName) throws URISyntaxException, IOException {
+        return csvService.checkBestPictureByName(movieName);
+    }
 }
--- movieAPI/src/main/java/com/movie/movieapi/utils/Navigation.java
+++ movieAPI/src/main/java/com/movie/movieapi/utils/Navigation.java
@@ -3,4 +3,5 @@ package com.movie.movieapi.utils;
 public class Navigation {
 
     public static final String MOVIE = "/api/movie/";
+    public static final String CSVFILE = "academy_awards.csv";
 }
